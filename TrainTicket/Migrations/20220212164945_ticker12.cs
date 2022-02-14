1599cbc2142d3bc48ea05c68120342d681a30c53
snpsujon <snpsujon93@gmail.com>
2022-02-14 11:01:12 +0600

    Add Feature

---
+++ TrainTicket/Migrations/20220212164945_ticker12.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrainTicket.Migrations
+{
+    public partial class ticker12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PerTicketPrice",
+                table: "ticketInformations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PerTicketPrice",
+                table: "ticketInformations");
+        }
+    }
+}
